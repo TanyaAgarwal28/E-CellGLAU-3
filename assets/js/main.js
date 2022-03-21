97cf0462f00e6c1e9613a04a7940f70f180671b5
TanyaAgarwal28 <tanya.agrawal1_cs19@gla.ac.in>
2022-03-21 21:30:24 +0700

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -393,6 +393,7 @@ function showSlides(n) {
 
 
 
+
 });
 
 
@@ -408,5 +409,6 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();*/
+
 
 
